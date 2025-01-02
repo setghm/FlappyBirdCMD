813992ea158d1fc3451dad6c672c3322543e7913
Set HM <setghm@gmail.com>
2025-01-01 18:43:12 -0600

    Export self-extracting CAB file

---
+++ Export.ps1
@@ -0,0 +1,52 @@
+# Export file for FlappyBirdCMD
+#
+# setghm (c) 2025
+#
+# Generate a self-extracting Cabinet file.
+#
+param(
+	[Parameter(Mandatory=$true)][string]$TargetDir,
+	[Parameter(Mandatory=$true)][string]$ProjectDir,
+	[Parameter(Mandatory=$true)][string]$ProjectName
+)
+
+$ExportName = "flappy"
+$ExportCabName = "$ExportName.cab"
+$ExportExeName = "$ExportName.exe"
+$ExportDirectiveFile = "directives.ddf"
+
+$AutoExtractProgram = "$env:WINDIR\System32\expand.exe"
+
+# Work on the build directory.
+Set-Location $TargetDir
+
+# Get assets folder contents.
+$AssetsDir = "$($ProjectDir)assets"
+$AssetsList = Get-ChildItem $AssetsDir | ForEach-Object { "$AssetsDir\$($_.Name)" }
+
+# Create the cabinet directive file contents.
+$CabinetDirectiveFile = @"
+; Generated MakeCAB directive file
+;
+.OPTION EXPLICIT
+
+.Set CabinetNameTemplate=$ExportCabName
+.Set DiskDirectoryTemplate=.
+.Set CompressionType=MSZIP
+.Set Cabinet=on
+.Set Compress=on
+
+$TargetDir$ProjectName.exe
+
+.Set DestinationDir=assets
+$($AssetsList -join "`r`n")
+"@
+
+# Save Cabinet directive file.
+$CabinetDirectiveFile | Out-File -Encoding utf8 -FilePath $ExportDirectiveFile
+
+# Generate the Cabinet file on the build directory.
+MakeCAB /F $ExportDirectiveFile
+
+# Generate the self-extracting Cabinet file.
+Get-Content $AutoExtractProgram,$ExportCabName -Encoding Byte | Set-Content $ExportExeName -Encoding Byte
--- src/Classes/GameObjects/Score.cpp
+++ src/Classes/GameObjects/Score.cpp
@@ -16,40 +16,40 @@ Score::Score(void) : GameObject() {
 }
 
 void Score::draw(void) {
+	// Make a copy of score.
+	uint32_t score = this->score;
+
 	// Count score ciphers.
 	int ciphers = 0;
-	uint32_t s = score;
 
-	do {
-		s /= 10;
-		ciphers++;
-	} while (s > 0);
+	do { score /= 10; ciphers++; } while (score > 0);
 
 	// Calculate the total width.
-	const int number_width = static_cast<double>(numbers.size.width) / 10;
-	const int width = number_width * ciphers;
+	const double number_width = static_cast<double>(numbers.size.width) / 10.0;
+	const double number_height = static_cast<double>(numbers.size.height);
+	const double draw_width = number_width * ciphers;
 	
 	// Calculate the X position.
-	const double start_x = (config::screen::width - width) / 2;
-	const double final_x = start_x + width;
+	const double start_x = (config::screen::width - draw_width) / 2.0;
+	const double final_x = start_x + draw_width;
 
 	// Draw each number.
-	s = score;
+	score = this->score;
 
-	Rect2d draw_rect{ 0, 0, number_width, numbers.size.height };
-	Rect2d screen_rect{ 0, config::game::score_position_y, width, numbers.size.height };
+	Rect2d draw_rect{ 0, 0, number_width, number_height };
+	Rect2d screen_rect{ 0, config::game::score_position_y, draw_width, number_height };
 
 	for (int x = start_x, c = ciphers; c > 0; x += number_width, c--) {
 		const double value = std::pow(10, c - 1);
-		const int number = s / value;
-		s -= number * value;
+		const int number = score / value;
+		
+		score -= number * value;
 
 		draw_rect.x = number * number_width;
 		screen_rect.x = x;
 
 		Console::getInstance()->draw(numbers, draw_rect, screen_rect);
 	}
-
 }
 
 void Score::increment(void) {
